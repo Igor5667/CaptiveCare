7c527b3fb4bd0ab9087d43afb209bb530a664f87
Igor <Igor@gmal.com>
2024-05-04 16:21:36 +0200

    connect database

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+app.use(cors())
+
+const db = 'first01'
+const url = `mongodb://localhost:27017/${db}`
+
+mongoose.connect(url)
+    .then(()=>console.log(`Connected to database ${db}`))
+    .catch((err)=>console.log("Connection error: ", err.message))
+
+
+app.get("/api/users", async (req,res)=>{
+    try{
+        const users = await User.find({})
+        res.json(users)
+        
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+
+
+app.listen(PORT, ()=>console.log(`Server express is running ${PORT}`))
+
+process.on('SIGINT', ()=>{
+    console.log("Closing MongoDB")
+    mongoose.disconnect()
+        .then(()=>console.log("MongoDB connection closed"))
+        .finally(()=>process.exit())
+})
+
